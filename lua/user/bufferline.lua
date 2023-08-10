29d06e93df7399be132e89718bb982a4a16ad9ab
tuNovio <alfgomachi@gmail.com>
2023-08-10 12:42:35 -0500

    se quita código que genera interferencias

--- lua/user/bufferline.lua
+++ lua/user/bufferline.lua
@@ -8,26 +8,5 @@ require('bufferline').setup({
     warning_diagnostic_selected = { bold = true },
     error_selected = { bold = true },
     error_diagnostic_selected = { bold = true },
-  },
-  options = {
-    show_close_icon = false,
-    diagnostics = 'coc',
-    max_prefix_length = 8,
-    diagnostics_indicator = function(count, level, diagnostics_dict, context)
-      if context.buffer:current() then
-        return ''
-      end
-      if level:match('error') then
-        return ' ' .. vim.g.diagnostic_icons.Error
-      elseif level:match('warning') then
-        return ' ' .. "~^>"
-      end
-      return ''
-    end,
-    custom_filter = function(buf_number, buf_numbers)
-      if vim.bo[buf_number].filetype ~= 'oil' then
-        return true
-      end
-    end,
-  },
+  }
 })
