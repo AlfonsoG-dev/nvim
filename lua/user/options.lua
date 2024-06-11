58001ef87181f77a0cd4e6870dd617fdeeac16ce
AlfonsoG-dev <alfgomachi@gmail.com>
2024-06-10 23:04:58 -0500

    refactor: tab white space

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -34,3 +34,5 @@ vim.o.wildmode           = "longest,full"               -- Display auto-complete
 vim.o.updatetime         = 500                          -- Delay until write to Swap and HoldCommand event
 vim.o.timeoutlen         = 500
 vim.g.do_file_type_lua   = 1
+vim.opt.list             = true
+vim.opt.listchars:append('tab:> ')
