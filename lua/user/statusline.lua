a54659e701db75e4c34f7f8c08fdc9cf33a4ef7d
AlfonsoG-dev <alfgomachi@gmail.com>
2025-11-17 13:47:56 -0500

    refactor: better status line

--- lua/user/statusline.lua
+++ lua/user/statusline.lua
@@ -1,11 +1,29 @@
--- Statusline en Lua (ordenado y limpio)
+vim.api.nvim_set_hl(0, "SLGitBranch", { fg = "#89b4fa", bold = true })     -- Azul claro
+vim.api.nvim_set_hl(0, "SLGitChanges", { fg = "#f38ba8" })                -- Rojo suave
+vim.api.nvim_set_hl(0, "SLFile",       { fg = "#f9e2af" })                -- Verde
+vim.api.nvim_set_hl(0, "SLIcon",       { fg = "#a6e3a1" })                -- Amarillo
+vim.api.nvim_set_hl(0, "SLPosition",   { fg = "#cba6f7" })                -- Morado
+
 vim.o.statusline = table.concat({
-  ' ',                                            -- Icono decorativo
-  '%t',                                             -- Nombre del archivo con ruta relativa
-  ' [%{WebDevIconsGetFileTypeSymbol()}]',          -- Icono del tipo de archivo
-  '%m',                                             -- Indica si el buffer fue modificado [+]
-  ' %= ',                                           -- Separador central
-  ' %<',                                            -- Recorte si se excede el ancho
-  ' %y',                                            -- Tipo de archivo
-  ' | %l:%c '                                       -- Línea y columna
+  '%#SLIcon#', ' ',                        -- Icono decorativo con color
+  '%#SLFile#', '%t',                        -- Nombre del archivo
+  ' [%{WebDevIconsGetFileTypeSymbol()}]',   -- Icono del tipo de archivo
+  '%m',                                     -- Modificado [+]
+  ' %= ',                                   -- Separador central
+  ' %<',                                    -- Truncamiento
+
+  -- --- Git info (rama + cambios) ---
+  '%#SLGitBranch#',
+  ' %{len(FugitiveHead()) ? " " . FugitiveHead() : ""}',
+
+  '%#SLGitChanges#',
+  ' %{FugitiveStatusline()}',
+
+  -- --- Info del archivo ---
+  '%#SLFile#',
+  ' | %y',
+
+  -- --- Posición ---
+  '%#SLPosition#',
+  ' | %l:%c ',
 })
