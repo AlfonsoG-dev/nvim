37087c77e821dd5be72c7c620887f63fca36d2a4
AlfonsoG-dev <alfgomachi@gmail.com>
2025-11-29 11:15:00 -0500

    refactor: autocomplete color

--- lua/user/theme.lua
+++ lua/user/theme.lua
@@ -23,14 +23,5 @@ vim.api.nvim_create_autocmd("ColorScheme", {
 require('kanagawa').setup({
     commentStyle = {italic = true, bold = true},
     keywordStyle = {bold = true},
-    dimInactive = false,
-    overrides = function(colors)
-        local theme = colors.theme
-        return {
-            Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1, blend = vim.o.pumblend},
-            PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
-            PmenuSbar = { bg = theme.ui.bg_m1 },
-            PmenuThumb = { bg = theme.ui.bg_p2 },
-        }
-    end,
+    dimInactive = false
 })
